e3530afea26fcfa6a124729840c24f182b818ea2
1lch2 <1lch2@163.com>
2020-03-07 14:42:58 +1100

    resource update

---
+++ main.js
@@ -0,0 +1,32 @@
+function getJsonObject(path, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success) success(JSON.parse(xhr.responseText));
+            } else {
+                if (error) error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+// Load data into <table> element.
+function loadElements() {
+    bookList = []; // book list container
+    getJsonObject('data.json',
+        function(data) {
+            bookList = data; // store the book list into bookList
+            console.log(bookList); // print it into console (developer tools)
+            console.log(bookList[0]); // print the first book object into console 
+            // here you can call methods to laod or refresh the page 
+            // loadBooks() or refreshPage()
+        },
+        function(xhr) { console.error(xhr); }
+    );
+    //TODO: Insert json objects into <table>.
+}
+
+window.onload = loadElements();
