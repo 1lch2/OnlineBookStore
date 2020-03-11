e08ae856f33ff0159d329f35cdfa84de10b82df4
1lch2 <1lch2@163.com>
2020-03-11 17:02:32 +1100

    nodejs server in progress

--- js/backend.js
+++ js/backend.js
@@ -1,13 +1,44 @@
 const http = require('http');
 const querystring = require('querystring');
+const fs = require('fs');
 
 const port = 8888;
 
-const server = http.createServer(function(req, res){
-    // This is the parameters of the GET request.
-    var reqdata = querystring.parse(req.url.split('?')[1]);
+const server = http.createServer(function(request, response){
+    // Load HTML file.
+    var url = request.url.split('?')[0];
+    var requestdata = querystring.parse(request.url.split('?')[1]);
 
-    //TODO: process the request data.
-    //TODO: load HTML.
+    console.log('[request url]: ' + url);
 
-}).listen(port);
+    if(url === '/'){
+        response.writeHead(200,{'Content-Type':'text/html'});
+        fs.readFile('../index.html', 'utf-8', function(err, data){
+            if(err){throw err};
+            response.end(data);
+        });
+    }else if(url === '/index.html'){
+        response.writeHead(200,{'Content-Type':'text/html'});
+        fs.readFile('../index.html', 'utf-8', function(err, data){
+            if(err){throw err};
+            response.end(data);
+        });
+    }else if(url != '/'){ // Load static resource.
+        var surl = '..' + url
+        var type = surl.substr(surl.lastIndexOf(".")+1, surl.length)
+
+        response.writeHead(200,{'Content-type':"text/"+type});
+        fs.readFile(surl, function(err, data){
+            if(err){throw err};
+            response.end(data);
+        });
+    }
+
+    // Process GET request.
+    var stringobj = JSON.stringify(requestdata)
+    if( stringobj !== '{}'){
+        console.log('[GET request]: ' + stringobj);
+        //TODO: return info to browser.
+    }
+
+}).listen(port, '127.0.0.1', console.log('Server start at '+port));
