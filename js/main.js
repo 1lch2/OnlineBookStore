de3aa0899c9e366d99fd9e43946eaf56b6813df9
1lch2 <1lch2@163.com>
2020-03-14 17:26:47 +1100

    search implemented

--- js/main.js
+++ js/main.js
@@ -14,22 +14,22 @@ function getJsonObject(path, success, error) {
 }
 
 // Create table row from JSON object.
-function createRow(data){
+function createRow(data) {
     var table = document.getElementById('table');
 
     // Write table row.
-    for (var i in data){
+    for (var i in data) {
         var row = table.insertRow(-1);
-        row.setAttribute("class", "row"+i);
-        for(var j = 0; j < 9; j++){
+        row.setAttribute("id", "shelf" + i);
+        for (var j = 0; j < 9; j++) {
             var cell = row.insertCell(-1);
-            switch(true){
+            switch (true) {
                 case j == 0: // checkbox
                     cell.innerHTML = "<label><input type='checkbox' class='checkforcart'></label>";
                     cell.setAttribute("class", "checkbox");
                     break;
                 case j == 1: // book cover
-                    cell.innerHTML = "<img src='./"+ data[i]["img"] +"' class='book'>";
+                    cell.innerHTML = "<img src='./" + data[i]["img"] + "' class='book'>";
                     cell.setAttribute("class", "pic");
                     break;
                 case j == 2: // title
@@ -38,9 +38,9 @@ function createRow(data){
                     break;
                 case j == 3: // star
                     var starhtml = ""
-                    for(var k = 0; k < parseInt(data[i]["rating"]); k++){
+                    for (var k = 0; k < parseInt(data[i]["rating"]); k++) {
                         starhtml += "<img src='./images/star-16.ico'>"
-                    } 
+                    }
                     cell.innerHTML = starhtml;
                     cell.setAttribute("class", "star");
                     break;
@@ -66,7 +66,7 @@ function createRow(data){
                     break;
             }
         }
-    }        
+    }
 
 }
 
@@ -74,7 +74,7 @@ function createRow(data){
 function loadElements() {
     bookList = []; // book list container
     getJsonObject('../data.json',
-        function(data) {
+        function (data) {
             bookList = data; // store the book list into bookList
             console.log(bookList); // print it into console (developer tools)
             console.log(bookList[0]); // print the first book object into console 
@@ -83,21 +83,21 @@ function loadElements() {
             // loadBooks() or refreshPage()
             createRow(bookList);
         },
-        function(xhr) { console.error(xhr); }
+        function (xhr) { console.error(xhr); }
     );
 }
 
 // Filter items based on selection.
-function filterItem(){
+function filterItem() {
     var selection = document.getElementById("select");
     var rows = document.getElementsByTagName("tr").length - 1;
-    for (var i = 0; i < rows; i++){
+    for (var i = 0; i < rows; i++) {
         //TODO: add category into attributes.
     }
 }
 
 // Darkmode
-function darkMode(){
+function darkMode() {
     var body = document.body;
     var darkbox = document.getElementById("dark");
     var darklabel = document.getElementById("darkswitch");
@@ -108,16 +108,11 @@ function darkMode(){
     var cartnum = document.getElementById("cartnum");
     var select = document.getElementById("select");
 
-    if(checkstat){
+    if (checkstat) {
         // Background color
         body.style.backgroundColor = "black"
         container.style.backgroundColor = "#222222";
         container.style.color = "white";
-
-        // Hover
-        // var rows = document.getElementsByClassName()
-        //TODO: hover
-        //TODO: highlight
 
         // Input box, select, switch, image and cart number background color
         cartimg.style.backgroundColor = "grey";
@@ -130,7 +125,7 @@ function darkMode(){
         cartnum.style.backgroundColor = "#222222";
         cartnum.style.color = "white"
 
-    }else if(!checkstat){
+    } else if (!checkstat) {
         body.style.backgroundColor = "white"
 
         container.style.backgroundColor = "rgb(236, 248, 243)";
@@ -151,50 +146,89 @@ function darkMode(){
 }
 
 // Add to cart
-function addCart(){
+function addCart() {
     var boxes = document.querySelectorAll(".checkforcart");
     var selectnum = 0;
-    for(var i = 0; i < boxes.length; i++){
-        if(boxes[i].checked){
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].checked) {
             selectnum += 1;
         }
     }
-    if(selectnum > 1){
+    if (selectnum > 1) {
         alert("You can only choose one at a time!");
-        for(var i = 0; i < boxes.length; i++){
+        for (var i = 0; i < boxes.length; i++) {
             boxes[i].checked = false;
-        }    
-    }else if(selectnum == 1){
-        var num = prompt("How many do you want?",1);
+        }
+    } else if (selectnum == 1) {
+        var num = prompt("How many do you want?", 1);
         var cartnum = document.getElementById("cartnum");
         totalnum = parseInt(cartnum.innerHTML) + parseInt(num); // Add up total number.
         cartnum.innerHTML = totalnum;
-        for(var i = 0; i < boxes.length; i++){
+        for (var i = 0; i < boxes.length; i++) {
             boxes[i].checked = false;
-        }    
-    }else if(selectnum == 0){
+        }
+    } else if (selectnum == 0) {
         alert("You have not selected any book.");
     }
-}        
+}
 
-function resetCart(){
+function resetCart() {
     var choose = confirm("Do you wish to clear your cart?");
-    if (choose){
+    if (choose) {
         var cartnum = document.getElementById("cartnum");
         cartnum.innerHTML = 0;
     }
 }
 
 // Highlight items based on search.
-function highlightItem(){
-    //TODO: highlight
+function highlightItem(indexlist) {
+    for (var i =0; i < indexlist.length; i++) {
+        var rowid = "shelf" + indexlist[i];
+        document.getElementById(rowid).style.backgroundColor = "yellow";
+    }
 }
 
-function search(){
-    //TODO: serach
+function search() {
+    var form = document.getElementById("form");
+
+    form.onsubmit = function (e) {
+        var searchtext = document.getElementById("searchbox");
+        var titlelist = document.querySelectorAll(".title");
+    
+        if (searchtext.value.length > 0) { // Check if the input is empty.
+            var indexlist = [];
+            for (var i = 0; i < titlelist.length; i++) {
+                var currenttitle = titlelist[i].innerHTML;
+                // Check if search text occurs in one of the titles.
+                if (currenttitle.indexOf(searchtext.value) != -1) {
+                    indexlist.push(i);
+                }
+            }
+            // Highlight the corresponding row.
+            highlightItem(indexlist);
+            e.preventDefault();
+        } else {
+            alert("You have not typed anything.");
+            e.preventDefault();
+        }
+    }
 }
+
+//TODO: Hover highlight: waiting to be finished.
+// function darkModeHover() {
+//     var darkbox = document.getElementById("dark");
+//     var checkstat = darkbox.checked;
+//     var rows = document.querySelectorAll(".shelf")
+//     if (checkstat) {
+//         rows.onmouserover = function () {
+//             this.style.backgroundColor = "#222222";
+//         }
+//     }
+
+// }
+
 
 // Onload event.
-window.onload = function(){
+window.onload = function () {
     this.loadElements();
 }
